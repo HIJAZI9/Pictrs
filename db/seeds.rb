526a0b1677f5b4c896e58aa1a7949e36922d7342
Mohammed Hijazi <mohammedhijazi@MacBook-Air-von-Mohammed.local>
2025-03-27 00:44:54 +0100

    Set Up the Database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# Create a default user
+User.create(username: "Christian", password: "345")
